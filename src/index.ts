d3783c8d6f068079dd74ef33292524d0f645bd4a
Gavin King <plumpnation@gmail.com>
2021-08-17 10:47:18 +0200

    working example with ts

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { DataSet, Network } from 'vis-network/standalone';
+
+// create an array with nodes
+const nodes = new DataSet([
+  { id: 1, label: "Node 1" },
+  { id: 2, label: "Node 2" },
+  { id: 3, label: "Node 3" },
+  { id: 4, label: "Node 4" },
+  { id: 5, label: "Node 5" },
+]);
+
+// create an array with edges
+const edges = new DataSet([
+  { from: 1, to: 3 },
+  { from: 1, to: 2 },
+  { from: 2, to: 4 },
+  { from: 2, to: 5 },
+  { from: 3, to: 3 },
+]);
+
+// create a network
+const container = document.getElementById("mynetwork");
+const data = {
+  nodes: nodes,
+  edges: edges,
+};
+
+const options = {};
+
+const network = new Network(container, data, options);
